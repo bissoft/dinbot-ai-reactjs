a625bb2a6a8230fe33c74cd85bd401255c55e6cf
muhammad-zubair-khan <mohammadzubairkhan007@gmail.com>
2023-12-05 12:00:19 +0500

    fix header & sidebar

--- src/Components/Layouts/Base.jsx
+++ src/Components/Layouts/Base.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 function Base(props) {
-
- 
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
   return (
     <div className="dashboard-container">
-      <div className="sidebar-container"> 
-        <Sidebar />
+      <div className="sidebar-container">
+        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       </div>
-      <div className={`main-content`}>
-       <Header/>
-        <Outlet/>
+      <div className={`main-content ${isSidebarOpen ? "" : "closed-sidebar"}`}>
+        <Header />
+        <Outlet />
       </div>
     </div>
   );
