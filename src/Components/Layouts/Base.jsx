eb080bac5b704d7500e70f41bb5339f56d25570f
umeransari7424 <umeransari7424@gmail.com>
2023-12-05 15:43:54 +0500

    api integration

--- src/Components/Layouts/Base.jsx
+++ src/Components/Layouts/Base.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom'
 function Base(props) {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
   };
+ 
   return (
     <div className="dashboard-container">
-      <div className="sidebar-container">
+      <div className="sidebar-container"> 
         <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} isAuthenticated={props.isSignedIn} onLogout={props.signout} onLogin={props.signin} onRemove={props.removeLogo}/>
       </div>
-      <div className={`main-content`}>
-       <Header/>
+      <div className={`main-content ${isSidebarOpen ? "" : "closed-sidebar"}`}>
+       <Header isAuthenticated={props.isSignedIn} onLogout={props.signout} onLogin={props.signin} onRemove={props.removeLogo}/>
         <Outlet/>
       </div>
     </div>
